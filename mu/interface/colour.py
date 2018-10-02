c20bbd33c581d4805e5fcf4d846b5d318f261bbb
Zander Brown <zbrown@gnome.org>
2018-10-02 22:06:04 +0100

    custom-all-the-things: Store buttons in list

--- mu/interface/colour.py
+++ mu/interface/colour.py
@@ -35,10 +35,10 @@ class ColourButton(QPushButton):
     Button for picking a colour
     """
 
-    def __init__(self, parent, colour):
+    def __init__(self, parent):
         super().__init__(parent)
         # Cache the inital colour
-        self._colour = colour
+        self._colour = None
         # We create dialogs as needed
         self.dlg = None
         # Respond to clicks
@@ -46,8 +46,6 @@ class ColourButton(QPushButton):
 
         # Format string for setting background
         self.sheet = "QPushButton {{ background: {}; }} "
-        # Set initial colour
-        self.preview(colour)
 
     def activated(self, checked=None):
         # If we haven't already show a dialog
@@ -95,101 +93,38 @@ class ColourWidget(QWidget):
         self.layout.setColumnStretch(1, 1)
         self.layout.setColumnStretch(3, 1)
         self.setLayout(self.layout)
-        # Foreground
-        col = QColor(255, 0, 0)
-        self.foreground = self.add_button(0, 0, _("Foreground"), col)
-        # Background
-        col = QColor(0, 255, 0)
-        self.background = self.add_button(0, 2, _("Background"), col)
-        # Editor Text
-        col = QColor(0, 255, 0)
-        self.editor_fore = self.add_button(1, 0, _("Editor Text"), col)
-        # Editor Background
-        col = QColor(0, 255, 0)
-        self.editor_back = self.add_button(1, 2, _("Editor Background"), col)
-        # Border
-        col = QColor(0, 0, 255)
-        self.border = self.add_button(2, 0, _("Border"), col)
-        # Control
-        col = QColor(0, 255, 0)
-        self.control = self.add_button(2, 2, _("Buttons"), col)
-        # Hover
-        col = QColor(0, 255, 0)
-        self.hover = self.add_button(3, 0, _("Hover"), col)
-        # Focus
-        col = QColor(0, 255, 0)
-        self.focus = self.add_button(3, 2, _("Focus"), col)
-        # Current Tab
-        col = QColor(0, 255, 0)
-        self.tab_current = self.add_button(4, 0, _("Current Tab"), col)
-        # Close
-        col = QColor(0, 255, 0)
-        self.close = self.add_button(4, 2, _("Close"), col)
+        self.controls = []
+        self.add_button(0, 0, _("Foreground"), "FOREGROUND")
+        self.add_button(0, 2, _("Background"), "BACKGROUND")
+        self.add_button(1, 0, _("Editor Text"), "EDITOR-FOREGROUND")
+        self.add_button(1, 2, _("Editor Background"), "EDITOR-BACKGROUND")
+        self.add_button(2, 0, _("Border"), "BORDER")
+        self.add_button(2, 2, _("Buttons"), "CONTROL")
+        self.add_button(3, 0, _("Hover"), "HOVER")
+        self.add_button(3, 2, _("Focus"), "FOCUS")
+        self.add_button(4, 0, _("Current Tab"), "TAB-CURRENT")
+        self.add_button(4, 2, _("Close"), "CLOSE")
 
-    def add_button(self, row, col, label, current):
-        btn = ColourButton(self, current)
+    def add_button(self, row, col, label, id):
+        btn = ColourButton(self)
         self.layout.addWidget(btn, row, col, Qt.AlignLeft)
         lbl = QLabel(label)
         self.layout.addWidget(lbl, row, col + 1, Qt.AlignLeft)
+        self.controls.append((btn, id))
         return btn
 
     def get_colours(self, default):
         res = {}
 
-        # TODO: Find a less verbose way to do this
-        if self.background.colour != default["BACKGROUND"]:
-            res["BACKGROUND"] = self.background.colour
-
-        if self.foreground.colour != default["FOREGROUND"]:
-            res["FOREGROUND"] = self.foreground.colour
-
-        if self.editor_back.colour != default["EDITOR-BACKGROUND"]:
-            res["EDITOR-BACKGROUND"] = self.editor_back.colour
-
-        if self.editor_fore.colour != default["EDITOR-FOREGROUND"]:
-            res["EDITOR-FOREGROUND"] = self.editor_fore.colour
-
-        if self.border.colour != default["BORDER"]:
-            res["BORDER"] = self.border.colour
-
-        if self.control.colour != default["CONTROL"]:
-            res["CONTROL"] = self.control.colour
-
-        if self.hover.colour != default["HOVER"]:
-            res["HOVER"] = self.hover.colour
-
-        if self.focus.colour != default["FOCUS"]:
-            res["FOCUS"] = self.focus.colour
-
-        if self.tab_current.colour != default["TAB-CURRENT"]:
-            res["TAB-CURRENT"] = self.tab_current.colour
-
-        if self.close.colour != default["CLOSE"]:
-            res["CLOSE"] = self.close.colour
+        for btn, id in self.controls:
+            if btn.colour != default[id]:
+                res[id] = btn.colour
 
         return res
 
     def set_colours(self, colours, fallback):
-        self.background.colour = self.get_colour(
-            colours, fallback, "BACKGROUND"
-        )
-        self.foreground.colour = self.get_colour(
-            colours, fallback, "FOREGROUND"
-        )
-        self.editor_back.colour = self.get_colour(
-            colours, fallback, "EDITOR-BACKGROUND"
-        )
-        self.editor_fore.colour = self.get_colour(
-            colours, fallback, "EDITOR-FOREGROUND"
-        )
-        self.border.colour = self.get_colour(colours, fallback, "BORDER")
-        self.control.colour = self.get_colour(colours, fallback, "CONTROL")
-        self.hover.colour = self.get_colour(colours, fallback, "HOVER")
-        self.focus.colour = self.get_colour(colours, fallback, "FOCUS")
-        self.tab_current.colour = self.get_colour(
-            colours, fallback, "TAB-CURRENT"
-        )
-        self.close.colour = self.get_colour(colours, fallback, "CLOSE")
+        for btn, id in self.controls:
+            btn.colour = self.get_colour(colours, fallback, id)
 
     def get_colour(self, colours, fallback, name):
         colour = fallback[name]
